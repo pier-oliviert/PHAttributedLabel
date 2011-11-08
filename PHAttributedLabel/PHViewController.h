b679b14acc1a73abf343a64b4f0b6e08f82bd435
Pier-Olivier Thibault <pothibo@gmail.com>
2011-11-08 08:35:54 -0500

    Initial Commit

---
+++ PHAttributedLabel/PHViewController.h
@@ -0,0 +1,13 @@
+//
+//  PHViewController.h
+//  PHAttributedLabel
+//
+//  Created by Pier-Olivier Thibault on 11-11-08.
+//  Copyright (c) 2011 25th Avenue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHViewController : UIViewController
+
+@end
---
+++ PHAttributedLabel/PHViewController.m
@@ -0,0 +1,60 @@
+//
+//  PHViewController.m
+//  PHAttributedLabel
+//
+//  Created by Pier-Olivier Thibault on 11-11-08.
+//  Copyright (c) 2011 25th Avenue. All rights reserved.
+//
+
+#import "PHViewController.h"
+
+@implementation PHViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
